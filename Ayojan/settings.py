0ca02c3516dbd0105b75eb0bb970476ed8fb3c4a
Pushkar Chaturvedi <131661389+PushkarOM@users.noreply.github.com>
2024-09-20 23:23:40 +0530

    Update settings.py

--- Ayojan/settings.py
+++ Ayojan/settings.py
@@ -152,7 +152,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build','staticfiles')
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build','static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Static files (CSS, JavaScript, Images)
