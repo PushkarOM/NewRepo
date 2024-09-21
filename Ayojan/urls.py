6501422c3254b283e578a6549958b25370e782f4
PushkarOM <pushkarchaturvedi42@gmail.com>
2024-09-21 14:57:56 +0530

    urls.py changes

--- Ayojan/urls.py
+++ Ayojan/urls.py
@@ -27,3 +27,9 @@ urlpatterns = [
     path('payment/',include('Payment.urls'))
 
 ]
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
